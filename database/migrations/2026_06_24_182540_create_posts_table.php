3a820279de6937f47d24dbb1ccc6bdeae4f67ed0
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-06-25 00:08:52 +0100

    Modifier la table créée

--- database/migrations/2026_06_24_182540_create_posts_table.php
+++ database/migrations/2026_06_24_182540_create_posts_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+
+
+            $table->foreignId('user_id')
+              ->constrained()
+              ->onDelete('cascade');
+
+        $table->text('content');
             $table->timestamps();
         });
     }
