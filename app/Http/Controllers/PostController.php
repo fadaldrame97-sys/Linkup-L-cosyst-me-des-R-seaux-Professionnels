1393c6eaade8c3f2eacea80707e7d195c3ab0dd1
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-06-25 21:32:50 +0100

    j'ai appeller le controller dans le routeur

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Post;
 
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
  public function index (){
-    $post=Post::with('user')->latest()->get();
+    $posts=Post::with('user')->latest()->get();
 
     return view('feed',compact('posts'));
  }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/feed', function () {
-    return view('feed');
-});
+//Route::get('/feed', function () {
+    //return view('feed')});
+
+
+    Route::get('/feed',[PostController::class,'index']);
