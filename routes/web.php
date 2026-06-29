5ccb6d6f813b6f6a2bc312964d6a924ccfe9bb7b
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-06-29 14:23:25 +0100

    Créer un fichier AuthController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/feed', function () {
-    return view('feed');
-});
+
+//Route::get('/feed', function () {
+    //return view('feed');
+//});
+
+Route::get('/feed',[PostController::class,'index'])
+->name('feed');
