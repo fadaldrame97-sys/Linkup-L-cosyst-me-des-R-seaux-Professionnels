3d333e169a18c3eaa9b5f7c9d616fc5ed6c9eb04
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-06-29 15:09:01 +0100

    Créer deux méthodes pour afficher le form register et login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function showRegisterForm(){
+
+        return view('auth.register');
+
+    }
+
+    public function showLoginForm(){
+
+        return view('auth.login');
+
+    }
 }
