973795cda0343ffc8fa4facef30d7fec4fa46b48
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-06-30 02:25:07 +0100

    Créer user et redirction vers feed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
 use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 
 
@@ -24,10 +27,23 @@ class AuthController extends Controller
 
     public function register(RegisterRequest $request)
     {
-    $data = $request->validated();
+    $data = $request->validated(
+
+    );
+    
 
    //dd($request->validated());
 
      return redirect()->route('register.form')->with('success','Inscription réuissi');
-    }
+    
+    $user=User::create(attributes: [
+        'name'=>$data['name'],
+        'email'=>$data['email'],
+        'password'=>Hash::make(value:$data['password'])
+
+    ]);
+      auth()->login($user);
+     return redirect('/feed')->with('success', 'Bienvenue sur LinkUp !');
+
+     }
     }
--- app/Http/requests/RegisterRequest.php
+++ app/Http/requests/RegisterRequest.php
@@ -1,10 +1,10 @@
 <?php
-namespace App\Http\Requests;
-
 
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+
+
 
 class RegisterRequest extends FormRequest
 {
@@ -19,7 +19,7 @@ class RegisterRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'unique:users,email'],
-            'headline' => ['required', 'string', 'max:255'],
+        
             'password' => ['required', 'confirmed', 'min:8'],
         ];
     }
