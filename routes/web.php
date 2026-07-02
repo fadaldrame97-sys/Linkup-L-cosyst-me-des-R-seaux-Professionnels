f9b7ce1c56216096eb4ac4463f4888131d687490
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-02 12:26:23 +0100

    Créer la route de l'edit

--- routes/web.php
+++ routes/web.php
@@ -4,21 +4,27 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-// Feed
+
 Route::get('/feed', [PostController::class, 'index'])->name('feed');
 
-// Register
+
+
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register.form');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 
-// Login
+
+
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.form');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-// Logout
+
+
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
 
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 
 
 Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+
+Route::put('/posts/{id}',[PostController::class,'update'])->name('posts.update');
