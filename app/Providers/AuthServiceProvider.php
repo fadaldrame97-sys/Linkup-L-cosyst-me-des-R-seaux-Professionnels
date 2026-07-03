113921b772e508f86d2cfa07ff6fda53f5f55173
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-03 10:24:58 +0100

    Créer les providers pour les policies

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Post;
+use App\Policies\PostPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    protected $policies = [
+        Post::class => PostPolicy::class,
+    ];
+
+    public function boot(): void
+    {
+        $this->registerPolicies();
+    }
+}
