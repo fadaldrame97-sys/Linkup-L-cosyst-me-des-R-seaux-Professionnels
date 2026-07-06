903f8833fa836472b97b419c8b02136382d4ffd1
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-06 15:19:52 +0100

    Créer les fichier de comments

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
 <div class="max-w-md mx-auto mt-10 bg-white p-6 rounded shadow">
 
     <h2 class="text-2xl font-bold mb-6 text-center">
-        Créer un compte
+        Connectez-vous
     </h2>
 
    <form action="{{ route('login') }}" method="POST">
