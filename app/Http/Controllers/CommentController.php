2ab464626682253b410a45f65e65649c07ed0431
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-06 17:55:03 +0100

    Terminer la création d'un commentaire

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\StoreCommentRequest;
+
+use Illuminate\Http\Request;
+use App\Models\Comment;
+use App\Models\Post;
 class CommentController extends Controller
 {
-    //
+     public function store(StoreCommentRequest $request, Post $post){
+     $data=$request->validated();
+
+     Comment::create([
+      'content'=>$data['content'],
+       'user_id'=>Auth::id(),
+       'post_id'=>$post->id,
+     ]);
+     
+     return redirect()->route('feed');
+ }
+
 }
