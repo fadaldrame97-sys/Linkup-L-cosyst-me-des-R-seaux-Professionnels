6ca73d604c1fb86e754fd502b698fdfcd6af55d4
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-07-08 09:50:09 +0100

    Créer toggle like

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+//use resources\view\feed.bl
+use Illuminate\Support\Facades\Auth;
+
 use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Models\Like;
 
 class LikeController extends Controller
 {
-    //
+       public function  toggle(Post $post){
+     $like=Like::where('user_id',auth::id())->where('post_id',$post->id)->fist();
+     if($like){
+        $like->delete();
+
+     }
+     else{
+        Like::create([
+            'user_id'=>auth::id(),
+            'post_id'=>$post->id,
+
+        ]);
+     }   return redirect()->route('feed');
+   }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -23,4 +23,6 @@ class Post extends Model
     {
     return $this->belongsToMany(Post::class, 'likes');
    }
+
+
 }
